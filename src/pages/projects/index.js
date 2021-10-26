2d01fe69737dad676be0c942a0896588d3d4cbb0
wedhimd <wedhimd@gmail.com>
2021-10-26 17:12:58 +0200

    add projects page

---
+++ src/pages/projects/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from "../../components/Layout"
+import  "../../sass/projects.scss"
+
+
+export default function Projects() {
+    return (
+        <Layout>
+            <div className="portfolio">
+                <h2>Portfolio</h2>
+                <h3>Projects and websites I've Created</h3>
+            </div>
+        </Layout>
+    )
+}
