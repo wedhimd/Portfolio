8287711015516b2ddd05fc9d930757132bb8d112
wedhimd <wedhimd@gmail.com>
2021-10-26 16:41:23 +0200

    add about page and change home styles

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function about() {
+    return (
+        <div>
+            <h2>this is the about page</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt architecto dolor omnis, odio velit repellendus cumque eaque consectetur adipisci vero, nesciunt pariatur quam aspernatur, repellat ut similique blanditiis officiis obcaecati?</p>
+        </div>
+    )
+}
